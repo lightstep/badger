49b5a58948dc917403e084e85d6ab8f1288f9c49
Ben Brown <bbrown@lightstep.com>
2019-10-29 10:47:55 -0700

    copy: factor out

--- badger/cmd/copy.go
+++ badger/cmd/copy.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
-	"github.com/dgraph-io/badger"
+	"fmt"
+
+	"github.com/dgraph-io/badger/badger/cmd/impl"
 	"github.com/spf13/cobra"
 )
 
@@ -26,49 +28,8 @@ func init() {
 }
 
 func doCopy(cmd *cobra.Command, args []string) error {
-	// Open the source DB.
-	source, err := badger.Open(
-		badger.DefaultOptions(sstDir).
-			WithValueDir(vlogDir).
-			WithTruncate(true))
-	if err != nil {
-		return err
+	if len(args) > 0 {
+		return fmt.Errorf("no extra args allowed: %v", args)
 	}
-	defer source.Close()
-
-	// Open the destination DB.
-	dest, err := badger.Open(badger.DefaultOptions(outputDir))
-	if err != nil {
-		return err
-	}
-	defer dest.Close()
-
-	// Do the copy.
-	return source.View(func(txn *badger.Txn) error {
-		iter := txn.NewIterator(badger.IteratorOptions{
-			AllVersions: false,
-		})
-		defer iter.Close()
-		for iter.Rewind(); iter.Valid(); iter.Next() {
-			err := func() error { // Provides tighter scope for the defer inside.
-				batch := dest.NewWriteBatch()
-				defer batch.Cancel()
-
-				val, err := iter.Item().ValueCopy(nil)
-				if err != nil {
-					return err
-				}
-
-				err = batch.Set(iter.Item().Key(), val)
-				if err != nil {
-					return err
-				}
-				return batch.Flush()
-			}()
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
+	return impl.Copy(sstDir, vlogDir, outputDir)
 }
